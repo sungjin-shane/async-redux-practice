4bfa5fb66de49d5e8cb598bbd9f829964db27ef4
sungjin-shane <sjpark731015@gmail.com>
2018-07-04 11:47:56 +1200

    add input text and I could search news by the text

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,48 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-    Fetch Posts
-  </button>
-)
+class LoadSubreddit extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      newSubreddit: ''
+    }
+
+    this.updateSubreddit = this.updateSubreddit.bind(this)
+    this.callDispatch = this.callDispatch.bind(this)
+  }
+
+  updateSubreddit (event) {
+    console.log(event.target.value)
+    this.setState({
+      newSubreddit: event.target.value
+    })
+  }
+
+  callDispatch (event) {
+    console.log(this.state.newSubreddit)
+    this.props.dispatch(fetchPosts(this.state.newSubreddit))
+    // this.props.dispatch(fetchPosts('newzealand'))
+  }
+
+  render () {
+    return (
+      <div>
+        <input type="text" name="newSubreddit" onChange={this.updateSubreddit}/>
+        <button onClick={this.callDispatch}>Fetch Posts
+        </button>
+      </div>
+    )
+  }
+
+// const LoadSubreddit = ({dispatch}) => (
+//   <div>
+//     <input type="text" name="newSubreddit" onChange={() => dispatch(fetchPosts('newzealand'))}/>
+//     <button onClick={() => dispatch(fetchPosts('newzealand'))}>
+//     Fetch Posts
+//     </button>
+//   </div>
+// )
+}
 
 export default connect()(LoadSubreddit)
